8d5f7249c24c6e9084bc0aa0a83fc833ce43434d
Sarah hagen <sarahhagen@sarahs-mbp.home>
2023-02-15 17:57:42 +0000

    add hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography, Button } from '@mui/material';
 
 const HeroBanner = () => {
-    return(
-        <div>HeroBanner</div>
+    return (
+        <Box sx={{
+            mt: { lg: '212px', xs: '70px' },
+            ml: { sm: '50px' }
+        }} postion="relative" p="20px">
+
+            <Typography color='#FF2625' fontWeight='600' fontSize='26px'>Fitness Videos</Typography>
+
+            <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      Get Fit, Have Fun <br />
+      And Repeat
+    </Typography>
+
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+      Check out the most effective exercises
+    </Typography>
+    <Button variant="contained" color='error'>
+        View Videos
+    </Button>
+
+        </Box>
     )
 }
 
